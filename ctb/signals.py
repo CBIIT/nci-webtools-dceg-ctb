51ad415b80a35af254fd1949925692e7c06abfc7
eleeisb <elee@systemsbiology.org>
2024-02-16 00:17:44 -0800

    bugfix

--- ctb/signals.py
+++ ctb/signals.py
@@ -20,7 +20,7 @@ from pytz import timezone
 from django.conf import settings
 from django.core.mail import EmailMessage
 from django.contrib.auth.signals import user_login_failed, user_logged_in
-from allauth.account.signals import password_changed, email_confirmed, password_reset
+from allauth.account.signals import password_changed, email_confirmed, password_reset, user_signed_up
 from django.dispatch import receiver
 from allauth.account.decorators import verified_email_required
 
@@ -101,6 +101,14 @@ def email_confirmed_callback(sender, email_address, **kwargs):
 def user_logged_in_callback(*args, **kwargs):
     try:
         logger.info(f"[CTB LOGIN] user_logged_in_callback")
+    except Exception as e:
+        logger.exception(e)
+
+
+@receiver(user_signed_up)
+def user_signed_up_callback(sender, request, user, **kwargs):
+    try:
+        logger.info(f'[CTB SIGN UP] A new user account \'{user}\' was created')
     except Exception as e:
         logger.exception(e)
 
